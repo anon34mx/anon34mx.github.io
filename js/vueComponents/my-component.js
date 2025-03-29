9685ce6a53a2ff3e9236db1166bf74ec10ccf28c
aaron <a.non34@hotmail.com>
2025-03-29 11:40:22 -0600

    vue test
    plantilla_vue.html

---
+++ js/vueComponents/my-component.js
@@ -0,0 +1,9 @@
+// // my-component.js
+// import { ref } from './composition-api@1.7.2'
+// export default {
+//     setup() {
+//         const count = ref(0)
+//         return { count }
+//     },
+//     template: `<div>Count is: {{ count }}</div>`
+// }
